a9ddce4cbba77b360887899ff5786c5617a18c92
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-05-12 18:34:41 -0700

    remove client from pool

--- lab-taylor/lib/client-pool.js
+++ lab-taylor/lib/client-pool.js
@@ -4,20 +4,36 @@ const EventEmitter = require('events').EventEmitter;
 
 function registerClient(socket, clientPool){
   socket.wack = {};
-  socket.wack.id = 'client_' + Date.now();
+  socket.wack.id = 'user_' + Date.now();
   clientPool.pool[socket.wack.id] = socket;
+}
+
+function broadcast(data, clientPool) {
+  Object.keys(clientPool.pool).forEach((clientId) => {
+    clientPool.pool[clientId].write(data.toString());
+  });
+}
+
+function removeClient(socket, clientPool) {
+
+  Object.keys(clientPool.pool).forEach( (clientId) => {
+    if (socket.wack.id === clientId) {
+      console.log(`removing ${socket.wack.id} from the pool`);
+      delete clientPool[clientId];
+    }
+  });
 }
 
 function registerClientListeners(socket, clientPool){
   socket.on('data', (data) => {
     console.log(data.toString());
-    Object.keys(clientPool.pool).forEach((clientId) => {
-      clientPool.pool[clientId].write(data.toString());
-    });
+    broadcast(data, clientPool);
   });
 
   socket.on('close', () => {
     console.log('a client has disconnected');
+    removeClient(socket, clientPool);
+
   });
 
   socket.on('error', (err) => {
