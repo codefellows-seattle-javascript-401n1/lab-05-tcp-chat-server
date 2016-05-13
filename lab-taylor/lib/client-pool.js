791abd0f98215456b94cc0fe84a43e60061e1d60
TAYLOR WIRTZ <taylor.wirtz@gmail.com>
2016-05-12 20:00:41 -0700

    \nick command working

--- lab-taylor/lib/client-pool.js
+++ lab-taylor/lib/client-pool.js
@@ -16,17 +16,47 @@ function registerClient(socket, clientPool){
   clientPool.pool[socket.wack.id] = socket;
 }
 
-function broadcast(data, user, clientPool) {
+function broadcast(data, socket, clientPool) {
   Object.keys(clientPool.pool).forEach((clientId) => {
-    clientPool.pool[clientId].write(`${user}:: ${data.toString()}`);
+    clientPool.pool[clientId].write(`${socket.wack.nickname}:: ${data.toString()}`);
   });
 }
+
+function parseCommands(data, socket, clientPool) {
+  const commands = {
+    '\\nick': updateNickname,
+    '\\pugbomb': pugBomb
+  };
+
+  if (data.toString()[0] === '\\') {
+    let cmd = data.toString().split(' ');
+    console.log('cmd', cmd);
+    if (commands[cmd[0]]){
+      return commands[cmd[0]](cmd.slice(1).join(' '), socket, clientPool);
+    } else {
+      return broadcast('command not found\n', socket, clientPool);
+    }
+  }
+
+  broadcast(data, socket, clientPool);
+}
+
+const updateNickname = function(newNickname, socket, clientPool){
+  clientPool.pool[socket.wack.id].wack.nickname = newNickname.trim();
+  broadcast(`${socket.wack.id} has a new nickname: ${newNickname.trim()}\n\n`, socket, clientPool);
+  return clientPool;
+};
+
+const pugBomb = function(socket) {
+  //something cool here
+  return socket;
+};
 
 function removeClient(socket, clientPool) {
   Object.keys(clientPool.pool).forEach( (clientId) => {
     if (socket.wack.id === clientId) {
       console.log(`removing ${socket.wack.nickname} from the pool`);
-      delete clientPool[clientId];
+      delete clientPool.pool[clientId];
     }
   });
 }
@@ -34,7 +64,7 @@ function removeClient(socket, clientPool) {
 function registerClientListeners(socket, clientPool){
   socket.on('data', (data) => {
     console.log(`${socket.wack.nickname}:: ${data.toString()}`);
-    clientPool.emit('broadcast', data, socket.wack.nickname, clientPool);
+    clientPool.emit('broadcast', data, socket, clientPool);
   });
 
   socket.on('close', () => {
@@ -53,13 +83,14 @@ const ClientPool = module.exports = function () {
   this.pool = {};
 
   this.on('register', (socket) => {
-    socket.write('selcome to wack!\n');
+    socket.write('welcome to wack!\n\n');
     registerClient(socket, this);
     registerClientListeners(socket, this);
   });
 
   this.on('broadcast', (data, user, pool) => {
-    broadcast(data, user, pool);
+  //  broadcast(data, user, pool);
+    parseCommands(data, user, pool);
   });
 };
 
