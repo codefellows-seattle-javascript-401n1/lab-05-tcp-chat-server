9eea8f7f87459e015b9cf1330d67d6848592110f
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-05-13 08:28:28 -0700

    add nicname change event

--- lab-anthony/lib/client-pool.js
+++ lab-anthony/lib/client-pool.js
@@ -5,13 +5,20 @@ const uuid = require('node-uuid');
 
 function registerClient(socket, clientpool) {
   socket.chat = {};
-  socket.chat.nick = 'guest_'+clientpool.guests;
-  socket.chat.id = 'client_' + uuid.v4();
+  socket.chat.nick = 'guest_'+ Math.floor(Math.random() * (1000 - 1)) + 1;
+  socket.chat.id = 'user_' + uuid.v4();
   clientpool.pool[socket.chat.id] = socket;
 }
 
 function registerClientListeners(socket, clientpool) {
   socket.on('data', (data) => {
+    if ((data.toString()).indexOf('\\nick') > -1){
+      var firstNick = socket.chat.nick;
+      clientpool.emit('newnick', data, socket);
+      var changedName = firstNick +' changed nickname to '+ socket.chat.nick;
+      clientpool.emit('broadcast', changedName, socket);
+      return;
+    }
     clientpool.emit('broadcast', data, socket);
   });
 
@@ -28,20 +35,22 @@ function registerClientListeners(socket, clientpool) {
 const ClientPool = module.exports = function() {
   EventEmitter.call(this);
   this.pool = {};
-  this.guests = 0;
 
   this.on('register', (socket) => {
     socket.write('Welcome new client!\n');
-    this.guests++;
     registerClient(socket, this);
     registerClientListeners(socket, this);
   });
 
   this.on('broadcast', (data, socket) => {
-    // console.log(clientpool.pool);
     Object.keys(this.pool).forEach((clientId)=>{
       this.pool[clientId].write(socket.chat.nick +': '+ data.toString());
     });
+  });
+
+  this.on('newnick', (data, socket) => {
+    var string = data.toString();
+    socket.chat.nick = (string.slice(6)).replace(/\r?\n|\r/,'');
   });
 };
 
