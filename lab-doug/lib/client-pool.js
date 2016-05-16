ed990fe51ca38b8ae12038a4e48a09212ff08ace
Doug Popadince <gsmatth@icloud.com>
2016-05-15 23:47:18 -0700

    updated register client wack.id

--- lab-doug/lib/client-pool.js
+++ lab-doug/lib/client-pool.js
@@ -4,7 +4,7 @@ const EventEmitter = require('events').EventEmitter;
 
 function registerClient(socket, clientPool){
   socket.wack = {};
-  socket.wack.id = 'user_' + Date.now();
+  socket.wack.id = 'user_' + uuid.v4();
   socket.wack.nickname = 'something';
   clientPool.pool[socket.wack.id] = socket;
 }
@@ -21,7 +21,7 @@ function registerClientListeners(socket, clientPool){
   socket.on('close', function(socket, clientPool){
     delete clientPool[socket.wack.id];
     //remove client from pool, you have access to socket.wack.id
-    //so just delete that property from clientpool
+    //so just delete that specific property from clientPool
   });
 
   socket.on('error', function(err){
@@ -30,7 +30,7 @@ function registerClientListeners(socket, clientPool){
 }
 
 const ClientPool = module.exports = function(){
-  EventEmitter.call(this);
+  EventEmitter.call(this);/*inherting the eventEmmitter PROPERTIES*/
   this.pool = {};
   this.on('register', (socket)=> {
     socket.write('welcome sluggy!\n');//write back to source of socket
@@ -38,8 +38,10 @@ const ClientPool = module.exports = function(){
     registerClientListeners(socket, this);//this is wackPool
   });
   this.on('broadcast', function(socket, message){
-  Object.keys(clientPool.pool).forEach(function(clientId){
-  clientPool.pool[clientId].write(data.toString());
+    Object.keys(clientPool.pool).forEach(function(clientId){
+      clientPool.pool[clientId].write(data.toString());
+    });
+  });
 };
 
-ClientPool.prototype = Object.create(EventEmitter.prototype);
+ClientPool.prototype = Object.create(EventEmitter.prototype);//inherting all the eventEmmitter METHODS
