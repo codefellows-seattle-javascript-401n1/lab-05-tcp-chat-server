5c9fe2be68d070f4709b69f6b7fc7e0f7658dca3
Jacob Knaack <jacobknaack@internalcheck.apple.com>
2016-05-15 20:57:03 -0700

    added comments

--- lab-jacob/server.js
+++ lab-jacob/server.js
@@ -5,14 +5,14 @@ const port = process.argv[2] || 3000;
 const ClientPool = require('./lib/client-pool');
 const clientPool = new ClientPool();
 
-const server = net.createServer();
+const server = net.createServer(); // creates our TCP server
 
 
 server.on('connection', function(socket){ //event waiting for connection
   clientPool.emit('register', socket);
 });
 
-server.on('error', function(err){
+server.on('error', function(err){ //listens for server error
   console.error('SERVER ERROR: ', err.message);
 });
 
