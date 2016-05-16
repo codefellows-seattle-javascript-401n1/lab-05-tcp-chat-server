11caefdbf0336aa1101faf9ce895a5b9fe618a17
YUN JOO KIM <moniqueyj282@hotmail.com>
2016-05-15 23:17:17 -0700

    take Ans white boarding

--- lab-yunjoo/lib/client-pool.js
+++ lab-yunjoo/lib/client-pool.js
@@ -1,24 +1,33 @@
 'use strict';
 
 const EventEmitter = require('events').EventEmitter;
+const uuid = require('node-uuid');
 
 function registerClient(socket, chatPool){
   socket.chat = {};
-  socket.chat.id = 'client_'+Date.now();
+  socket.chat.nick= 'guest_'+Math.floor(Math.rando()*(100-1))+1;
+  socket.chat.id = 'user_'+uuid.v4();
   chatPool.pool[socket.chat.id] = socket;
 
 }
 
 function registerClientListeners(socket, chatPool){
-  socket.on('data', function(data){
-    console.log(data.toString());
-    Object.keys(chatPool.pool).forEach(function(clientId){
-      chatPool.pool[clientId].write(data.toString());
-    });
+  socket.on('data', function(data) => {
+    if((data.toString()).indexOf('\\nick')>-1){
+      var firstNick = socket.chat.nick;
+      chatpool.emit('newnick', data, socket);
+      var changeName = firstNick + ' changed nickname to '+socket.chat.nick;
+      chatpool.emit('broadcast', changedName, socket);
+      return ;
+    }
+    chatpool.emit('broadcast',data, socket);
+    // console.log(data.toString());
+
   });
 
-  socket.on('close', function(){
-    console.log('a client has disconnected');
+  socket.on('close', ()=> {
+    console.log(socket.chat.id+' has disconnected');
+    delete chatpool.pool[socket.chat.id];
   });
   socket.on('error', function(err){
     console.error('CLIENT ERROR: ', err.message);
@@ -29,9 +38,19 @@ const ClientPool = module.exports = function(){
   EventEmitter.call(this);
   this.pool = {};
   this.on('register', (socket)=>{
+    socket.write('Welcome to the chat room!\n');
     registerClient(socket, this);
-    socket.write(this.pool[socket.chat.id]+'\n');
+    // socket.write(this.pool[socket.chat.id]+'\n');
     registerClientListeners(socket, this);
+  });
+  this.on('broadcast', (data, socket) => {
+    Object.keys(this.pool).forEach((clientId)=>{
+    chatPool.pool[clientId].write(socket.chat.nick +': ' + data.toString());
+  });
+});
+  this.on('newnick', (data,socket)=>{
+  var string = data.toString();
+  socket.chat.nick = (string.slice(6)).replace(/\r?\n|\r/,'');
   });
 };
 
