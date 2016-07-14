820d6320711774a999ea0ece37ae790621674a93
Scott Schmidt <sjschmidt44@gmail.com>
2016-07-13 20:57:23 -0700

    Add server and client-pool

--- lab-scott/lib/client-pool.js
+++ lab-scott/lib/client-pool.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const uuid = require('node-uuid');
+const EventEmitter = require('events').EventEmitter;
+
+const ClientPool = module.exports = function() {
+  EventEmitter.call(this);
+  this.pool = {};
+
+  this.on('register', (socket) => {
+    createClient(socket, this);
+    socket.write('Hello ' + socket.wack.id + '\n');
+    createClientListener(socket, this);
+  });
+
+  this.on('broadcast', (data) => {
+    Object.keys(this.pool).forEach((id) => {
+      this.pool[id].write(data.toString());
+    });
+  });
+};
+
+ClientPool.prototype = Object.create(EventEmitter.prototype);
+
+function createClient(socket, clientPool) {
+  socket.wack = {};
+  socket.wack.id = 'client_' + uuid.v1();
+  socket.wack.nick = 'guest_' + ~~(Math.random() * (100 -1) + 1);
+  clientPool.pool[socket.wack.id] = socket;
+}
+
+function createClientListener(socket, clientPool) {
+  socket.on('data', (data) => { clientPool.emit('broadcast', socket.wack.nick + ': ' + data); });
+  socket.on('close', () => { delete clientPool.pool[socket.wack.id]; });
+  socket.on('error', (err) => { console.error('Client Error:', err.message); });
+}
--- lab-scott/server.js
+++ lab-scott/server.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const net = require('net');
+const port = process.argv[2] || 3000;
+const ClientPool = require('./lib/client-pool');
+const wackPool = new ClientPool();
+const server = net.createServer();
+
+server.on('connection', (socket) => { wackPool.emit('register', socket); });
+server.on('error', (err) => { console.error('Server Error', err.message); });
+server.listen(port, () => { console.log('Server running on port:', port); });
