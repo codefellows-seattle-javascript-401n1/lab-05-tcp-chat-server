2d432047d4353f1c817ed7f02e2bd734bb080e0f
Scott Schmidt <sjschmidt44@gmail.com>
2016-07-23 15:10:34 -0700

    Update dm, and listUsers to work correctly.

--- lab-scott/lib/client-pool.js
+++ lab-scott/lib/client-pool.js
@@ -13,8 +13,8 @@ const ClientPool = module.exports = function() {
     socket.write('Hello ' + socket.wack.nick + '\n');
   });
 
-  this.on('broadcast', (data) => {
-    parseCommand(data, this);
+  this.on('broadcast', (data, socket) => {
+    parseCommand(data, socket, this);
   });
 };
 
@@ -29,18 +29,23 @@ function createClient(socket, clientPool) {
 
 function createClientListener(socket, clientPool) {
   socket.on('error', (err) => { console.error('Client Error:', err.message); });
-  socket.on('data', (data) => { clientPool.emit('broadcast', socket.wack.nick + ': ' + data); });
+  socket.on('data', (data) => { clientPool.emit('broadcast', socket.wack.nick + ': ' + data, socket); });
   socket.on('close', () => {
     clientPool.pool[socket.wack.id].end();
     delete clientPool.pool[socket.wack.id];
   });
 }
 
-function parseCommand(data, clientPool) {
-  let dataString = data.toString().split(' ');
+function parseCommand(data, socket, clientPool) {
+  let dataString = data.toString().split(' ') || null;
+  let nick = dataString[2] ? dataString[2].split('\n') : null;
   if (dataString[1].match(/^\/nick/ig) && dataString[2]) {
-    let nick = dataString[2].split('\n');
-    changeNick(data, clientPool, nick[0]);
+    changeNick(clientPool, socket, nick[0]);
+  } else if (dataString[1].match(/^\/dm/ig) && dataString[2]) {
+    let data = socket.wack.nick + ': ' + dataString.splice(3).join(' ');
+    directMsg(data, clientPool, nick);
+  } else if (dataString[1].match(/^\/users/ig)) {
+    listUsers(clientPool);
   } else {
     Object.keys(clientPool.pool).forEach((id) => {
       clientPool.pool[id].write(data.toString());
@@ -48,9 +53,26 @@ function parseCommand(data, clientPool) {
   }
 }
 
-function changeNick(data, clientPool, newNick) {
+function changeNick(clientPool, socket, newNick) {
+  clientPool.pool[socket.wack.id].wack.nick = newNick;
+  clientPool.pool[socket.wack.id].write('nick changed to ' + newNick + '\n');
+}
+
+function directMsg(data, clientPool, dmNick) {
   Object.keys(clientPool.pool).forEach((id) => {
-    clientPool.pool[id].wack.nick = newNick;
-    clientPool.pool[id].write('nick changed to ' + newNick);
+    if (clientPool.pool[id].wack.nick === dmNick.toString()) {
+      clientPool.pool[id].write(data.toString());
+    }
+  });
+}
+
+function listUsers(clientPool) {
+  let users = [];
+  Object.keys(clientPool.pool).forEach((id) => {
+    users.push(clientPool.pool[id].wack.nick + ' ');
+  });
+
+  Object.keys(clientPool.pool).forEach((id) => {
+    clientPool.pool[id].write('Users: ' + users.toString() + '\n');
   });
 }
